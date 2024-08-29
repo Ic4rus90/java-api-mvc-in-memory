2b959a2d22b51c3d5927548951f1f5a86539ca53
Thomas Wiik <thomas.wiik@no.experis.com>
2024-08-29 12:22:27 +0200

    Added comments for own learning. Same with declaring what exception is thrown in the method signature.

--- src/main/java/com/booleanuk/api/products/ProductRepository.java
+++ src/main/java/com/booleanuk/api/products/ProductRepository.java
@@ -16,7 +16,13 @@ public class ProductRepository {
         this.productList = new ArrayList<>();
     }
 
-    public Product create(ProductRequest productRequest){
+    public Product create(ProductRequest productRequest) throws ResponseStatusException {
+        /*
+        ResponseStatusException in method signature is not strictly necessary in this context.
+        This is because it is a subclass of RuntimeException, which means it is an unchecked exception (subclass of RuntimeException).
+        However, it serves the purpose of letting the developer implementing the function know what exceptions can be thrown.
+        Checked exceptions are checked at compile-time. Compiler needs the developer to handle these exceptions, by using try-catch or method signature.
+         */
         if (!nameIsInUse(productRequest.getName())){
             Product product = new Product(productRequest);
             this.productList.add(product);
@@ -35,7 +41,7 @@ public class ProductRepository {
         return this.productList;
     }
 
-    public List<Product> getProductsFromCategory(String productCategory){
+    public List<Product> getProductsFromCategory(String productCategory) throws ResponseStatusException {
         List<Product> productsInCategory = productList.stream()
                 .filter(p->p.getCategory().equalsIgnoreCase(productCategory))
                 .collect(Collectors.toCollection(ArrayList::new));
@@ -46,7 +52,7 @@ public class ProductRepository {
         }
     }
 
-    public Product getProductByID(int productID){
+    public Product getProductByID(int productID) throws ResponseStatusException {
         // Highly unnecessary, but fun.
         if (productID == 418){
             throw new ResponseStatusException(HttpStatus.I_AM_A_TEAPOT,  "<img src='https://media2.giphy.com/media/ARmZmMqobLtZKrJRrU/200w.gif?cid=6c09b95287okszcbz59ao584wd835y08z53ju0u1pwjpok20&ep=v1_gifs_search&rid=200w.gif&ct=g'>");
@@ -62,7 +68,7 @@ public class ProductRepository {
     }
 
 
-    public Product update(int productID, ProductRequest productRequest){
+    public Product update(int productID, ProductRequest productRequest) throws ResponseStatusException {
 
        if (!nameIsInUse(productRequest.getName())) {
            Product product = getProductByID(productID);
